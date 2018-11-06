626eae982e937adc273cdef20068d22e7d882453
carlosv4150 <carlosv4150@hotmail.com>
2018-11-05 22:40:22 -0500

    multiple browsers (#25)

--- protractor/bs.config.ts
+++ protractor/bs.config.ts
@@ -1,24 +1,38 @@
 import { Config, browser } from 'protractor';
 import { reporter } from './helpers/reporter';
 
-export const config: Config = {
+const firefoxConfig = {
+  browserName: 'firefox',
+  platform: 'mac',
+  name: 'firefox-tests',
+  shardTestFiles: true,
+  maxInstances: 1
+};
+
+const chromeConfig = {
+  browserName: 'chrome',
+  name: 'chrome-tests',
+  shardTestFiles: true,
+  maxInstances: 1,
+  chromeOptions: {
+    args: ['disable-infobars=true', '--window-size=800,600'],
+    prefs: { credentials_enable_service: false }
+  }
+};
+
+const multiCapabilities = [chromeConfig, firefoxConfig];
+
+export let config: Config = {
+  multiCapabilities,
   framework: 'jasmine',
   specs: ['../test/**/locators.spec.js'],
   getPageTimeout: 30000,
   jasmineNodeOpts: {
     defaultTimeoutInterval: 120000
   },
-  browserstackUser: process.env.BS_USERNAME,
-  browserstackKey: process.env.BS_ACCESS_KEY,
+  browserstackUser: 'carlosvibanco1', // process.env.BS_USERNAME,
+  browserstackKey: 'whZJxUyWPxZUGzgx1DjK', // process.env.BS_ACCESS_KEY,
   SELENIUM_PROMISE_MANAGER: false,
-  capabilities: {
-    name: 'UI Workshop',
-    browserName: 'chrome',
-    chromeOptions: {
-      args: ['disable-infobars=true', '--window-size=800,600'],
-      prefs: { credentials_enable_service: false }
-    }
-  },
   onPrepare: () => {
     browser.ignoreSynchronization = true;
     browser.manage().timeouts().implicitlyWait(0);
